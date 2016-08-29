7e354fc4620236309c6a32b0608985ea4fd8addd
biharygergo <biharygergo@DESKTOP-LVDOVAJ>
2016-08-29 10:42:11 +0200

    Add project files.

---
+++ Tanfolyam1/PersonHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanfolyam1
+{
+    static class PersonHelper
+    {
+        //this kulcsszoval a list sajat fv-enek fogja tekinteni a mi fv-unket! magic
+        internal static IEnumerable<Person> Filter(this List<Person> list, string filter)
+        {
+            //List<Person> result = new List<Person>();
+
+            foreach (var person in list)
+            {
+                if (person.Name.Contains(filter))
+                    yield return person; //results.Add(person);
+                //yield break;
+            }
+
+
+            //return result;
+        }
+
+        internal static string Dump(this object o)
+        {
+            string result = "";
+            foreach (var prop in o.GetType().GetProperties())
+            {
+                result += prop.Name + "=" + prop.GetValue(o) + ", ";
+            }
+
+            result += ")";
+            return result;
+            {
+
+            }
+        }
+
+    }
+    }
+
---
+++ Tanfolyam1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanfolyam1;
+
+namespace Tanfolyam1
+{
+
+    class Person
+    {
+        // TRÜKK propfull, propf +TABTAB
+
+        //8 sor helyett csak két sort használ így
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+
+        
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var i = 1;
+            //var s = "Béla";
+
+            //TRÜKK
+            // var tomb = new { Name = "Aladár", Age = 5 };
+            //Console.WriteLine(tomb.Age);
+            // Console.WriteLine(tomb.GetType());
+            //Console.ReadKey();
+
+            //Person p = new Person();
+            //p.Age = 12;
+            //p.Name = "aladár";
+
+            //TRÜKK
+            //Person p2 = new Person() { Name = "Aladár", Age = 12 };
+
+
+            //TRÜKK
+            //List<Person> list = new List<Person>()
+            //{
+
+            //    new Person {Name="Youda", Age =563 },
+            //    new Person {Name="Luke", Age =32 },
+            //    new Person {Name="Obi-Wan", Age =56 }, //Lehet itt vesszőt használni :)
+            //};
+
+            //foreach (var person in PersonHelper.Filter(list, "a"))
+            //{
+            //    Console.WriteLine(person.Name);
+
+            //    //TRÜKK így lehet beleírni egyből, rövidebb
+            //    Console.WriteLine($"Person: {person.Name} ({person.Age})");
+            //}
+
+            Nullable<int> i;
+            i = 5;
+            Console.WriteLine(i);
+         
+        }
+    }
+}
